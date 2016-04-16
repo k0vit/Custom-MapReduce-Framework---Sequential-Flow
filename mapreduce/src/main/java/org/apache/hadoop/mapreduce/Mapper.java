a37d3864eab1626dba0fbb7d238969c2d583d8e6
Kovit Nisar <kovit@ccs.neu.edu>
2016-04-16 03:04:26 -0400

    mapper, reducer and context

--- mapreduce/src/main/java/org/apache/hadoop/mapreduce/Mapper.java
+++ mapreduce/src/main/java/org/apache/hadoop/mapreduce/Mapper.java
@@ -1,5 +1,22 @@
 package org.apache.hadoop.mapreduce;
 
-public class Mapper {
+public class Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
 
+	public class Context implements IContext<KEYIN,VALUEIN,KEYOUT,VALUEOUT> {
+
+		@Override
+		public void write(KEYOUT key, VALUEOUT value) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	protected void setup(Context context){};
+
+	@SuppressWarnings("unchecked")
+	protected void map(KEYIN key, VALUEIN value, Context context) {
+		context.write((KEYOUT) key, (VALUEOUT) value); 
+	}
+
+	protected void cleanup(Context context) {}
 }
--- mapreduce/src/main/java/org/apache/hadoop/mapreduce/Reducer.java
+++ mapreduce/src/main/java/org/apache/hadoop/mapreduce/Reducer.java
@@ -1,5 +1,24 @@
 package org.apache.hadoop.mapreduce;
 
-public class Reducer {
+public class Reducer<KEYIN,VALUEIN,KEYOUT,VALUEOUT> {
 
+	public class Context implements IContext<KEYIN,VALUEIN,KEYOUT,VALUEOUT> {
+
+		@Override
+		public void write(KEYOUT key, VALUEOUT value) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	protected void setup(Context context){};
+
+	@SuppressWarnings("unchecked")
+	protected void reduce(KEYIN key, Iterable<VALUEIN> values, Context context) {
+		for (VALUEIN value : values) {
+			context.write((KEYOUT) key, (VALUEOUT) value);
+		}
+	};
+
+	protected void cleanup(Context context) {}
 }
