f1c07ca3f4a500f109898f14715bac714852f20b
Kovit Nisar <kovit@ccs.neu.edu>
2016-04-16 18:12:16 -0400

    method contract change

--- mapreduce/src/main/java/neu/edu/utilities/Utilities.java
+++ mapreduce/src/main/java/neu/edu/utilities/Utilities.java
@@ -1,14 +1,18 @@
 package neu.edu.utilities;
 
 import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
+import neu.edu.mapreduce.common.Node;
 
 public class Utilities {
 
-	private static void readInstanceDetails() {
+	private static List<Node> readInstanceDetails() {
 		File instanceDetails = new File("");
 	}
 
-	private static void readClusterProperties() {
+	private static Properties readClusterProperties() {
 		// TODO Auto-generated method stub
 		
 	}
