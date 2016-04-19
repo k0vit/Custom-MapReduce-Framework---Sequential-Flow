e1f525e05b32959bcc87e83694a2fe004f2feab0
k0vit <kovit@ccs.neu.edu>
2016-04-19 16:16:54 -0400

    script files done

--- scpTo.sh
+++ scpTo.sh
@@ -2,6 +2,21 @@
 
 chmod 400 ec2key.pem
 
+sleep 30
+
+while IFS="=" read key value
+do
+	key="${key// }"
+        value="${value// }"
+
+        if [ "$key" == "Jar" ]
+        then
+                jar=$value
+ 	fi
+done < user.properties
+
+echo $jar
+
 # II - instance identifier
 # PRIP - private ip
 # PUIP - public ip
@@ -9,10 +24,7 @@ chmod 400 ec2key.pem
 
 while IFS="," read II PRIP PUIP NT
 do
-        if [ "$NT" == "C" ]
-        then
-                scp -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" client-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
-        else
-                scp -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" SortNode-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
-        fi
+	scp -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" mapreduce-0.0.1-SNAPSHOT-jar-with-dependencies.jar ec2-user@$PUIP:~/
+
+	scp -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $jar ec2-user@$PUIP:~/
 done < InstanceDetails.csv
--- sshToRun.sh
+++ sshToRun.sh
@@ -4,39 +4,34 @@ chmod 400 ec2key.pem
 
 while IFS="=" read key value
 do
-	key="${key// }"
+        key="${key// }"
         value="${value// }"
 
-        if [ "$key" == "AccessKey" ]
+        if [ "$key" == "Arguments" ]
         then
-                accesskey=$value
-        elif [ "$key" == "SecretKey" ]
+                args=$value
+	elif [ "$key" == "Main" ]
         then
-                secretkey=$value
-	elif [ "$key" == "Bucket" ]
-        then
-                clusterdetails=$value
-		clusterdetails+="/InstanceDetails.csv"
+               main=$value
         fi
-done < cluster.properties
+done < user.properties
 
-echo $accesskey
-echo $secretkey
-echo $clusterdetails
+echo $args
+echo $main
 
 # II - instance identifier
 # PRIP - private ip
 # PUIP - public ip
-# NT - Node Type S/C
+# NT - Node Type Master/Slave
 
 while IFS="," read II PRIP PUIP NT
 do
-        if [ "$NT" == "C" ]
+        if [ "$NT" == "M" ]
         then
-                clientip=$PUIP
+                masterip=$PUIP
         else
-                ssh -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ec2-user@$PUIP "java -jar ~/SortNode-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $clusterdetails $accesskey $secretkey 2>&1" > sortnode-$PUIP-output.txt &
+                ssh -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ec2-user@$PUIP "java -cp ~/*.jar:. neu.edu.mapreduce.slave.Slave 2>&1" > slave-$PUIP-output.txt &
         fi
 done < InstanceDetails.csv
 
-ssh -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ec2-user@$clientip "java -jar ~/client-0.0.1-SNAPSHOT-jar-with-dependencies.jar $1 $2 $clusterdetails $accesskey $secretkey 2>&1" > client-output.txt
+ssh -i ec2key.pem -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ec2-user@$masterip "java -cp ~/*.jar:. $main $args 2>&1" > master-$msaterip-output.txt
